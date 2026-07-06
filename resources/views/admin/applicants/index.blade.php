11dc0269255754f0a68a65314f43d11687654ebb
sanjay dahal <sanjay.dahal90@gmail.com>
2026-07-07 02:03:12 +0545

    Initial commit

--- resources/views/admin/applicants/index.blade.php
+++ resources/views/admin/applicants/index.blade.php
@@ -72,7 +72,7 @@
 @endsection
 
 @section('scripts')
-    @include('admin.includes.datatables-js')
+    @include('admin.includes.datatables-scripts')
     <script>
         $(document).ready(function() {
             $('#applicantsTable').DataTable({
